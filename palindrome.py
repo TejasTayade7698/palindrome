79b2c9a9bdaefe61e7df1c1a21597b3fecd22f50
Tejas Tayade <92956577+TejasTayade7698@users.noreply.github.com>
2021-12-29 03:02:53 -0500

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def isPalindrome(self, x):  
+        
+        if x<0 or (x>0 and x%10==0):
+            return False         
+        
+        result=0
+        while x>result:
+                result=result*10+x%10
+                x=x//10
+        return True if (x==result or x==result//10) else False
+            
+             
+       
+   
+      
+        
+        """
+        :type x: int
+        :rtype: bool
+        """
+       
